babe834f1600b37873b6ebbd38a9c3fe57ef6725
marcelo <marcelocerqueira661@gmail.com>
2019-05-07 19:28:03 -0300

    premiro projeto

---
+++ App.js
@@ -0,0 +1,33 @@
+
+
+import React, {Component}from 'react';
+import { View,Text,Button,Alert,Image, TextInput,StyleSheet } from 'react-native';
+import{createStackNavigator,createAppContainer}from 'react-navigator';
+
+
+
+class HomeScreen extends Component {
+    render (){
+      return(
+        <View>style={{flex:1, justifyContemt: 'center'}}
+          <text> style={{fontSize:50}}>Tela Inicial</text>
+
+</View>
+      );
+      }
+    }
+const AppNavigator = createStackNavigator(
+  {
+Home:{
+  screen: HomeScreen
+}
+  }
+);
+
+const Appcontainer = createAppContainer(AppNavigator);
+export default class App extends Component {
+  render(){
+    return<Appcontainer/>;
+      
+  }
+  }
